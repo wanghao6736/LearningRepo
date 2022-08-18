8896fc7de68d7b50d4cdbf9f785d64becc9049bb
immortal6736 <immortal6736@qq.com>
2022-08-18 15:07:43 +0800

    fix bugs

--- TC Template/AnalysisTemplate.py
+++ TC Template/AnalysisTemplate.py
@@ -10,7 +10,7 @@ import shutil
 import itasca as it
 it.command("python-reset-state false")
 
-def full_csv_name():
+def get_ret_csv():
     for test in os.listdir('./ret/'):
         for ret in os.listdir('./ret/' + test):
             if ret == 'result.sav':
@@ -21,36 +21,39 @@ def full_csv_name():
                 src = os.path.join('.', 'ret', test, it.fish.get('test_name') + '.csv')
                 dst = os.path.join('.', 'csv', it.fish.get('test_name') + '.csv')
                 shutil.copy(src, dst)
-#full_csv_name()
 
-emods = []
-krats = []
-frics = []
-rrfrs = []
-emod, krat, fric, rrfr = 0.0, 0.0, 0.0, 0.0 # default value
-ini_model = 'BalancedSpecimen.sav'
+def run_tests():
+    emods = []
+    krats = []
+    frics = []
+    rrfrs = []
+    emod, krat, fric, rrfr = 0.0, 0.0, 0.0, 0.0 # default value
+    ini_model = 'BalancedSpecimen.sav'
 
-for emod in emods:
-    it.command(f"""
-    model restore {ini_model}
-    
-    [linEmod = {emod}]
-    [linKrat = {krat}]
-    [fric    = {fric}]
-    [rrFric  = {rrfr}]
-    
-    [g_s = 1e5 ] ; overwrite
-    [g_e = 1e-2] ; overwrite
-    [mem_name = 'example_mem']
-    [save_dir = './mem/' + mem_name + '_']
-    program call 'MemGen.p3dat' suppress
-    
-    [load_rate = 5e-2]
-    [test_name = 'example_test']
-    program system ['mkdir ret\\\\' + test_name]
-    [save_dir = './ret/' + test_name + '/']
-    program call 'Test.p3dat' suppress
-    
-    project save [global.title + '.prj']
-    program return
-    """)
+    for emod in emods:
+        it.command(f"""
+        model restore '{ini_model}'
+        
+        [linEmod = {emod}]
+        [linKrat = {krat}]
+        [fric    = {fric}]
+        [rrFric  = {rrfr}]
+        
+        [g_s = 1e5 ] ; overwrite
+        [g_e = 1e-2] ; overwrite
+        [mem_name = 'example_mem']
+        [save_dir = './mem/' + mem_name + '_']
+        program call 'MemGen.p3dat' suppress
+        
+        [load_rate = 5e-2]
+        [test_name = 'example_test']
+        program system ['mkdir ret\\\\' + test_name]
+        [save_dir = './ret/' + test_name + '/']
+        program call 'Test.p3dat' suppress
+        
+        project save [global.title + '.prj']
+        program return
+        """)
+
+# get_ret_csv()
+# run_tests()
